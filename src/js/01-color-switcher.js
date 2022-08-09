0b5cbf822413e84f17b74831868b009c7abd3b32
Alex-Taylor Danko <alexdankoxxl@gmail.com>
2022-08-09 17:10:03 +0300

    add task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,53 @@
+class ChangerColor {
+  constructor({ btnStartEl, btnStopEl, bodyEl }) {
+    this.btnStart = btnStartEl;
+    this.btnStop = btnStopEl;
+    this.color = bodyEl;
 
+    this.onChange = null;
+    this.btnStop.disabled = true;
+  }
+
+  start() {
+    this.btnToggle(this.btnStart);
+    this.btnToggle(this.btnStop);
+
+    this.onChange = setInterval(() => {
+      this.color.style.backgroundColor = this.getRandomHexColor();
+    }, 1000);
+  }
+
+  stop() {
+    this.btnToggle(this.btnStop);
+    this.btnToggle(this.btnStart);
+
+    clearInterval(this.onChange);
+  }
+
+  getRandomHexColor() {
+    let letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+
+  btnToggle(btn) {
+    btn.disabled = !btn.disabled;
+  }
+}
+
+const refs = {
+  btnStartEl: document.querySelector('button[data-start]'),
+  btnStopEl: document.querySelector('button[data-stop]'),
+  bodyEl: document.body,
+};
+
+const changerColor = new ChangerColor(refs);
+
+refs.btnStartEl.addEventListener(
+  'click',
+  changerColor.start.bind(changerColor)
+);
+refs.btnStopEl.addEventListener('click', changerColor.stop.bind(changerColor));
